48af9733977f86c0b3749abf724990889a08b0dc
EdLeafe <ed@leafe.com>
2014-01-17 15:54:19 -0600

    Fixed a bug in the header substitution. Also switched to spaces.

--- bounceDirect.py
+++ bounceDirect.py
@@ -1,8 +1,11 @@
+import email
 import getMsg
-import time
+import re
 import smtplib
+import time
 
 msg = getMsg.getMsg()
+
 fwdKey = "X-Forwarded-By: OT filtering script"
 okHdr1 = "X-Original-To: profoxxtproxy@leafe.com"
 okHdr2 = "X-Original-To: profox@leafe.com"
@@ -10,33 +13,37 @@ bounceHdr = "X-Forwarded-By: DIRECT POST"
 emptyHdr = "X-EMPTY-MESSAGE: True"
 ret = msg
 
-#ff = open("/var/dummyLogs/BD.LOG", "a")
-#ff.write("%s - %s (len=%s)\n" % (time.ctime(), msg.find(fwdKey), len(msg)))
+def logit(*args):
+    txt = " ".join(["%s" % arg for arg in args])
+    with open("/var/dummylogs/BD.LOG", "a") as ff:
+        logit("%s\n" % txt)
+
+logit("%s - %s (len=%s)\n" % (time.ctime(), msg.find(fwdKey), len(msg)))
 problem = False
+
 try:
-	if len(msg) == 0:
-#		ff.write("EMPTY MESSAGE - EXITING\n")
-		ret = emptyHdr + "\n"
-		problem = True
-#	ff.write("msg.find(fwdKey): %s - " % msg.find(fwdKey))
-	ok = (okHdr1 in msg) ###and (okHdr2 in msg)
-	if not ok:
-		# This is a direct post! Add the filter header
-		pos = msg.find("From: ")
-#		ff.write("POS: %s\n------msg-----------\n" % pos)
-#		ff.write("\n%s\n---------end---------\n\n" % msg)
-		ret = msg[:pos-1] + "\n" + bounceHdr + "\n" + msg[pos:]
-		problem = True
-#	if not problem:
-#		ff.write("\n------msg-----------\n")
-#		ff.write("\n%s\n---------end---------\n" % msg)
-#		ff.write("No problems found.\n")
+    if not msg:
+        logit("EMPTY MESSAGE - EXITING\n")
+        ret = emptyHdr + "\n"
+        problem = True
+    logit("msg.find(fwdKey): %s - " % msg.find(fwdKey))
+    ok = (okHdr1 in msg) ###and (okHdr2 in msg)
+    if not ok:
+        # This is a direct post! Add the filter header
+        eml = email.message_from_string(msg)
+        del eml["X-Forwarded-By"]
+        del eml["X-Forwarded-By"]
+        eml["X-Forwarded-By"] = "DIRECT POST"
+        msg = eml.as_string()
+        problem = True
+    if not problem:
+        logit("\n------msg-----------\n")
+        logit("\n%s\n---------end---------\n" % msg)
+        logit("No problems found.\n")
 except StandardError, e:
-	server = smtplib.SMTP("leafe.com")
-	server.sendmail("BounceDirect@leafe.com", "ed@leafe.com", 
-			"To: Ed Leafe <ed@leafe.com>\nSubject: Bounce Direct problem\n\nError encountered: %s\n\n%s" % (str(e), msg) )
-	server.quit()
+    server = smtplib.SMTP("leafe.com")
+    server.sendmail("BounceDirect@leafe.com", "ed@leafe.com", 
+            "To: Ed Leafe <ed@leafe.com>\nSubject: Bounce Direct problem\n\nError encountered: %s\n\n%s" % (str(e), msg) )
+    server.quit()
 
 print ret
-
-#ff.close()
